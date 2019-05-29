a644cf7831124cae81e621c41715c4f8f9a80212
apialeksandar <apialeksandar@yahoo.com>
2019-05-29 11:07:20 +0200

    dependency

--- WebApp/WebApp/Persistence/UnitOfWork/DemoUnitOfWork.cs
+++ WebApp/WebApp/Persistence/UnitOfWork/DemoUnitOfWork.cs
@@ -4,6 +4,7 @@ using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using Unity;
+using WebApp.Models;
 using WebApp.Persistence.Repository;
 
 namespace WebApp.Persistence.UnitOfWork
@@ -16,6 +17,29 @@ namespace WebApp.Persistence.UnitOfWork
         {
             _context = context;
         }
+
+        [Dependency]
+        public List<DayType> DayType { get; set; }
+        [Dependency]
+        public List<Line> Line { get; set; }
+        [Dependency]
+        public List<Pricelist> Pricelist { get; set; }
+        [Dependency]
+        public List<Station> Station { get; set; }
+        [Dependency]
+        public List<Ticket> Ticket { get; set; }
+        [Dependency]
+        public List<TicketPrice> TicketPrice { get; set; }
+        [Dependency]
+        public List<TicketType> TicketType { get; set; }
+        [Dependency]
+        public List<Timetable> Timetable { get; set; }
+        [Dependency]
+        public List<TimetableType> TimetableType { get; set; }
+        [Dependency]
+        public List<UserType> UserType { get; set; }
+        [Dependency]
+        public List<Vehicle> Vehicle { get; set; }
 
         public int Complete()
         {
